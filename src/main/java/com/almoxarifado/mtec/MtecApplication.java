b0c6dcb1a07bc3294091a068282615ab501375d2
EmberMisaka <embermisaka@gmail.com>
2026-08-04 17:00:46 -0300

    Projeto Almoxarifado MTec, apenas frontend

---
+++ src/main/java/com/almoxarifado/mtec/MtecApplication.java
@@ -0,0 +1,13 @@
+package com.almoxarifado.mtec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/almoxarifado/mtec/MtecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almoxarifado.mtec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
